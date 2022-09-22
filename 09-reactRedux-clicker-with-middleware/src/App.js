76728156eae192692aa8b6e0f1ec29103a876c39
Mmarvinn <cerkunik@gmail.com>
2022-09-22 14:57:56 +0300

    clicker with middleware and connect Redux DevTools

---
+++ 09-reactRedux-clicker-with-middleware/src/App.js
@@ -0,0 +1,28 @@
+import './App.css';
+import { useSelector, useDispatch } from 'react-redux';
+import { increment, decrement, reset } from './store';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello Redux</h1>
+      <Counter />
+    </div>
+  );
+}
+
+const Counter = () => {
+  const count = useSelector((state) => state);
+  const dispatch = useDispatch();
+
+  return (
+    <div>
+      <h2>{count}</h2>
+      <button onClick={() => dispatch(decrement)}>-</button>
+      <button onClick={() => dispatch(increment)}>+</button>
+      <button onClick={() => dispatch(reset)}>reset</button>
+    </div>
+  );
+};
+
+export default App;
