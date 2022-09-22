b6f72d1e584a134edbfc23bec61dc321ad5ebaec
Mmarvinn <cerkunik@gmail.com>
2022-09-22 13:23:39 +0300

    refactoring dashboard app without hooks, use connect()

---
+++ 08-dashboard-app-with-Class-Component/src/components/JobList.jsx
@@ -0,0 +1,31 @@
+import { connect } from 'react-redux';
+import { JobPosition } from './JobPosition';
+import { selectVisiblePositions } from 'store/positions/position-selectors';
+import { addFilter } from 'store/filters/filter-actions';
+
+const _JobList = ({ positions, addFilter }) => {
+  const handleAddFilter = (filter) => {
+    addFilter(filter);
+  };
+
+  return (
+    <div className="job-list">
+      {positions.map((item) => (
+        <JobPosition
+          key={item.id}
+          handleAddFilter={handleAddFilter}
+          {...item}
+        />
+      ))}
+    </div>
+  );
+};
+
+const mapStateToProps = (state) => ({
+  positions: selectVisiblePositions(state, state.filters),
+});
+const JobList = connect(mapStateToProps, {
+  addFilter,
+})(_JobList);
+
+export { JobList };
